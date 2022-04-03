efa17826337fe436cfa3be6c6829d76d74a23ee5
Arhontes <arhontes@yandex.ru>
2022-04-03 09:37:40 +0300

    Add project files.

---
+++ UsingAxaj_CodeAffection_DublicateProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UsingAxaj_CodeAffection_DublicateProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
