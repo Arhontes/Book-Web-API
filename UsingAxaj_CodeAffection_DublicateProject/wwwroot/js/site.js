cc7d309b6ebbc55b734b46580f3d4db390ef25c5
Arhontes <arhontes@yandex.ru>
2022-04-04 16:18:04 +0300

    Created a new ajax function

--- UsingAxaj_CodeAffection_DublicateProject/wwwroot/js/site.js
+++ UsingAxaj_CodeAffection_DublicateProject/wwwroot/js/site.js
@@ -9,3 +9,36 @@
     }
     })
 }
+
+
+jQueryAjaxPost = form => {
+    try {
+        $.ajax({
+            type: 'POST',
+            url: form.action,
+            data: new FormData(form),
+            contentType: false,
+            processData: false,
+            success: function (response) {
+                if (response.isValid) {
+                    $("#view-all").html(responce.html);
+                    $("#form-modal .modal-body").html('');
+                    $("#form-modal .modal-title").html('');
+                    $("#form-modal").modal('hide');
+                }
+                else
+                    $("#form-modal .modal-body").html(responce.html);
+            },
+            error: function (err) {
+                console.log(err)
+            }
+        })
+
+
+    } catch (e) {
+        console.log(e)
+
+    }
+    //to prevent default form submit event
+    return false;
+}
